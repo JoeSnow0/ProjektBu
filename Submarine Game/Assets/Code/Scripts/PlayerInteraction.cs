940058f2a934b29e065641de0858216a5b26225a
DESKTOP-Q9EB57K\Thomas <thomas>
2024-03-21 13:17:02 +0100

    Initial commit to phase 2 branch. Added material for walls, updated testing level. Added script for interactions.

---
+++ Submarine Game/Assets/Code/Scripts/PlayerInteraction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    //A script that fires a raycast in the direction the player is facing from the centre of the camera view.
+    //If it collides with anything interactable, get its name and display it on the UI
+    Camera mCam;
+
+}
