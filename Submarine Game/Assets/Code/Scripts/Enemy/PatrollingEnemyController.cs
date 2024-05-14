027809bb9f85c20a6a642b0e5aa24125c45e60d1
Thomas Richards <thomas.c.richards1@gmail.com>
2024-05-14 12:22:47 +0200

    added audioManager with volume control

--- Submarine Game/Assets/Code/Scripts/Enemy/PatrollingEnemyController.cs
+++ Submarine Game/Assets/Code/Scripts/Enemy/PatrollingEnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,11 +10,15 @@ using UnityEngine.AI;
 public class PatrollingEnemyController : EnemyController
 {
 
+    //public AudioSource mAudioSource;
     private void Start()
     {
         //mStates[0] is the default state
         mStates = GetComponents<EnemyStates>();
         mCurrentState = mStates[0].mState;
+
+        mAudioManager = FindObjectOfType<AudioManager>();
+        mAudioManager.PlaySound(1, mAudioSource, true);
     }
     private void Update()
     {
